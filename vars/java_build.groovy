fff913ab3f50ef6412e32f3bc33475ba59a47500
PS <pramilanarawade70@gmail.com>
2024-07-11 16:49:47 +0530

    Create java_build.groovy

---
+++ vars/java_build.groovy
@@ -0,0 +1,64 @@
+def call(Map config = [:]) {
+    pipeline {
+        agent any
+        environment {
+            DOCKERHUB_CREDENTIALS = credentials('dockerhub_credentials_id')
+            DOCKERHUB_USERNAME = "${DOCKERHUB_CREDENTIALS_USR}"
+            DOCKERHUB_PASSWORD = "${DOCKERHUB_CREDENTIALS_PSW}"
+        }
+        stages {
+            stage('Checkout') {
+                steps {
+                    checkout scm
+                }
+            }
+            stage('Build') {
+                steps {
+                    script {
+                        sh 'mvn clean package'
+                    }
+                }
+            }
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        sh 'docker build -t ${DOCKERHUB_USERNAME}/${config.imageName}:latest .'
+                    }
+                }
+            }
+            stage('Push Docker Image') {
+                steps {
+                    script {
+                        withDockerRegistry([credentialsId: 'dockerhub_credentials_id', url: 'https://index.docker.io/v1/']) {
+                            sh 'docker push ${DOCKERHUB_USERNAME}/${config.imageName}:latest'
+                        }
+                    }
+                }
+            }
+            stage('Cleanup') {
+                steps {
+                    cleanWs()
+                }
+            }
+        }
+        post {
+            always {
+                script {
+                    if (currentBuild.result != 'SUCCESS') {
+                        slackSend(
+                            channel: '#your-channel',
+                            color: '#FF0000',
+                            message: "Build failed: ${env.JOB_NAME} ${env.BUILD_NUMBER}"
+                        )
+                    } else {
+                        slackSend(
+                            channel: '#your-channel',
+                            color: '#00FF00',
+                            message: "Build succeeded: ${env.JOB_NAME} ${env.BUILD_NUMBER}"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
