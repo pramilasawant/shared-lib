fc6a1f0d9a98c4457bc3d3401a0dfeee6a45ea17
PS <pramilanarawade70@gmail.com>
2024-07-11 12:23:55 +0530

    Create build.groovy

---
+++ vars/build.groovy
@@ -0,0 +1,64 @@
+def call(Map config) {
+    pipeline {
+        agent any
+        environment {
+            DOCKERHUB_CREDENTIALS = ('dockerhubpwd')
+            DOCKERHUB_USERNAME = 'pramila188'
+        }
+        stages {
+            stage('Clone Repositories') {
+                parallel {
+                    stage('Clone Java Repo') {
+                        steps {
+                            git url: 'https://github.com/pramilasawant/springboot1-application.git', branch: 'main'
+                        }
+                    }
+                    stage('Clone Python Repo') {
+                        steps {
+                            git url: 'https://github.com/pramilasawant/phython-application.git', branch: 'main'
+                        }
+                    }
+                }
+            }
+            stage('Build Docker Images') {
+                parallel {
+                    stage('Build Java Docker Image') {
+                        steps {
+                            script {
+                                def javaImage = docker.build("${env.DOCKERHUB_USERNAME}/java-app")
+                                docker.withRegistry('', env.DOCKERHUB_CREDENTIALS) {
+                                    javaImage.push('latest')
+                                }
+                            }
+                        }
+                    }
+                    stage('Build Python Docker Image') {
+                        steps {
+                            script {
+                                def pythonImage = docker.build("${env.DOCKERHUB_USERNAME}/python-app")
+                                docker.withRegistry('', env.DOCKERHUB_CREDENTIALS) {
+                                    pythonImage.push('latest')
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            stage('Deploy to Kubernetes') {
+                steps {
+                    script {
+                        kubernetesDeploy(configs: 'k8s-deployment.yaml', kubeconfigId: 'kubeconfig-id')
+                    }
+                }
+            }
+        }
+        post {
+            success {
+                slackSend(channel: '#build-notifications', message: "Build and Deployment Successful: ${env.BUILD_URL}")
+            }
+            failure {
+                slackSend(channel: '#build-notifications', message: "Build and Deployment Failed: ${env.BUILD_URL}")
+            }
+        }
+    }
+}
