e5d4397c2d230a00c6a340d91dca10b47c974ee3
PS <pramilanarawade70@gmail.com>
2024-07-12 12:13:08 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,57 +1,110 @@
-def call(Map config) {
+def call(Map config = [:]) {
     pipeline {
         agent any
+
         environment {
-            DOCKERHUB_CREDENTIALS = ('dockerhubpwd')
-            DOCKERHUB_USERNAME = 'pramila188'
+            DOCKERHUB_CREDENTIALS = credentials('dockerhubpwd')
+            SLACK_CREDENTIALS = credentials('slackpwd')
         }
+
+        parameters {
+            string(name: 'JAVA_REPO', defaultValue: 'https://github.com/pramilasawant/springboot1-application.git', description: 'Java Application Repository')
+            string(name: 'PYTHON_REPO', defaultValue: 'https://github.com/pramilasawant/phython-application.git', description: 'Python Application Repository')
+            string(name: 'DOCKERHUB_USERNAME', defaultValue: 'pramila188', description: 'DockerHub Username')
+            string(name: 'JAVA_IMAGE_NAME', defaultValue: 'testhello', description: 'Java Docker Image Name')
+            string(name: 'PYTHON_IMAGE_NAME', defaultValue: 'python-app', description: 'Python Docker Image Name')
+            string(name: 'JAVA_NAMESPACE', defaultValue: 'test1', description: 'Kubernetes Namespace for Java Application')
+            string(name: 'PYTHON_NAMESPACE', defaultValue: 'python', description: 'Kubernetes Namespace for Python Application')
+        }
+
         stages {
             stage('Clone Repositories') {
-                parallel {
-                    stage('Clone Java Repo') {
-                        steps {
-                            git url: 'https://github.com/pramilasawant/springboot1-application.git', branch: 'main'
-                        }
-                    }
-                    stage('Clone Python Repo') {
-                        steps {
-                            git url: 'https://github.com/pramilasawant/phython-application.git', branch: 'main'
-                        }
-                    }
-                }
-            }
-            stage('Build Docker Images') {
-                parallel {
-                    stage('Build Java Docker Image') {
-                        steps {
-                            script {
-                                def javaImage = docker.build("${env.DOCKERHUB_USERNAME}/testhello")
-                                docker.withRegistry('', env.DOCKERHUB_CREDENTIALS) {
-                                    javaImage.push('latest')
-                                }
+                steps {
+                    parallel {
+                        stage('Clone Java Repo') {
+                            steps {
+                                git url: params.JAVA_REPO
                             }
                         }
-                    }
-                    stage('Build Python Docker Image') {
-                        steps {
-                            script {
-                                def pythonImage = docker.build("${env.DOCKERHUB_USERNAME}/python-app")
-                                docker.withRegistry('', env.DOCKERHUB_CREDENTIALS) {
-                                    pythonImage.push('latest')
+                        stage('Clone Python Repo') {
+                            steps {
+                                dir('python-app') {
+                                    git url: params.PYTHON_REPO
                                 }
                             }
                         }
                     }
                 }
             }
+
+            stage('Build and Push Docker Images') {
+                steps {
+                    parallel {
+                        stage('Build and Push Java Image') {
+                            steps {
+                                script {
+                                    def image = docker.build("${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}")
+                                    docker.withRegistry('', 'dockerhubpwd') {
+                                        image.push()
+                                    }
+                                }
+                            }
+                        }
+                        stage('Build and Push Python Image') {
+                            steps {
+                                dir('python-app') {
+                                    script {
+                                        def image = docker.build("${params.DOCKERHUB_USERNAME}/${params.PYTHON_IMAGE_NAME}")
+                                        docker.withRegistry('', 'dockerhubpwd') {
+                                            image.push()
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             stage('Deploy to Kubernetes') {
                 steps {
-                    script {
-                        kubernetesDeploy(configs: 'deploymentservice.yaml', kubeconfigId: 'k8sconfigpwd')
+                    parallel {
+                        stage('Deploy Java Application') {
+                            steps {
+                                script {
+                                    sh """
+                                    kubectl create namespace ${params.JAVA_NAMESPACE} || true
+                                    helm upgrade --install java-app helm/java-chart --namespace ${params.JAVA_NAMESPACE} \
+                                    --set image.repository=${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}
+                                    """
+                                }
+                            }
+                        }
+                        stage('Deploy Python Application') {
+                            steps {
+                                script {
+                                    dir('python-app') {
+                                        sh """
+                                        kubectl create namespace ${params.PYTHON_NAMESPACE} || true
+                                        helm upgrade --install python-app helm/python-chart --namespace ${params.PYTHON_NAMESPACE} \
+                                        --set image.repository=${params.DOCKERHUB_USERNAME}/${params.PYTHON_IMAGE_NAME}
+                                        """
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
+            }
+        }
+
+        post {
+            success {
+                slackSend channel: '#builds', color: 'good', message: "Build and Deployment of Java and Python applications succeeded."
+            }
+            failure {
+                slackSend channel: '#builds', color: 'danger', message: "Build and Deployment of Java and Python applications failed."
             }
         }
     }
 }
-      
